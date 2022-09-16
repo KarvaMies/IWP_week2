ef4324cb0c7a8cccdd1ded94953c6a6ea0b46e0d
KarvaMies <80526519+KarvaMies@users.noreply.github.com>
2022-09-16 14:37:12 +0300

    Task 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+
+const submitButton = document.getElementById("submit-data");
+
+submitButton.addEventListener("click", function () {
+  let users = document.getElementById("users");
+  let newRow = users.insertRow();
+
+  let username = newRow.insertCell();
+  username.innerHTML = document.getElementById("input-username").value;
+  //console.log(document.getElementById("input-username").value);
+
+  let email = newRow.insertCell();
+  email.innerHTML = document.getElementById("input-email").value;
+  //console.log(document.getElementById("input-email").value);
+
+  let address = newRow.insertCell();
+  address.innerHTML = document.getElementById("input-address").value;
+  //console.log(document.getElementById("input-address").value);
+
+  let admin = newRow.insertCell();
+  if (document.getElementById("input-admin").checked) {
+    admin.innerHTML = "X";
+  } else {
+    admin.innerHTML = "-";
+  }
+  //console.log(document.getElementById("input-admin").checked);
+});
